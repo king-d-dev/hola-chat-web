8a41640607fecf127b2d9023a6512500d1a98d3c
king-d-dev <davidoreal1@gmail.com>
2021-07-05 18:55:29 +0000

    completed the home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 function HomePage() {
-  return <div>Home Page</div>;
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <div className="home flex items-center justify-center bg-gray-100 h-screen m-auto">
+      <div className=" rounded-lg w-2/4 text-center flex flex-col items-center">
+        <h3 className="">
+          Welcome to hola chat, feel at home 🏡 and hit the button below when you ready to join the
+          conversation. 🥳
+        </h3>
+
+        <button
+          onClick={() => loginWithRedirect()}
+          className="mt-3 bg-gray-800 text-white px-6 py-2 rounded-sm"
+        >
+          login
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export { HomePage };
