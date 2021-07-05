40a224e9743a1e2728d5afdfbdff990e4e5627eb
king-d-dev <davidoreal1@gmail.com>
2021-07-05 17:25:16 +0000

    initial setup with tailwindcss

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare module 'react/jsx-runtime' {
+  import jsxRuntime from 'react/jsx-runtime';
+  export default jsxRuntime;
+}
+declare module 'react' {
+  import React from 'react';
+  export default React;
+}
