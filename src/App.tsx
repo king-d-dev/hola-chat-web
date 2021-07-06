f9dcfa1774f5592134e3c6a93c82a11bf9ec83e5
king-d-dev <davidoreal1@gmail.com>
2021-07-06 18:53:38 +0000

    implemented appropriate authentication /authorization for the chat page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
 import './assets/styles/index.css';
 import { Loading } from './components/loading';
 import { ChatPage } from './pages/chat';
 import { HomePage } from './pages/home';
+import { useAccessToken } from './stores/access-token';
 
 function App() {
-  const { isAuthenticated, isLoading } = useAuth0();
+  const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const { setAccessToken } = useAccessToken();
+
+  useEffect(() => {
+    async function retrieveAccessToken() {
+      if (!isAuthenticated) return;
+      const token = await getAccessTokenSilently();
+      // setAccessToken(token);
+      window.localStorage.setItem('access-token', token);
+      console.log('GETTING ACCESS TOKEN');
+    }
+    retrieveAccessToken();
+
+    // return () => window.localStorage.removeItem('access-token');
+  }, [getAccessTokenSilently, setAccessToken, isAuthenticated]);
 
   function renderContent() {
-    return <ChatPage />;
     if (isLoading) return <Loading />;
 
     if (isAuthenticated) return <ChatPage />;
