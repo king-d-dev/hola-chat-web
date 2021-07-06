e9272856638f90f6e852e09b4cfe05486c490b03
king-d-dev <davidoreal1@gmail.com>
2021-07-06 18:54:13 +0000

    removed unused code

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,7 +3,3 @@ declare module 'react/jsx-runtime' {
   import jsxRuntime from 'react/jsx-runtime';
   export default jsxRuntime;
 }
-declare module 'react' {
-  import React from 'react';
-  export default React;
-}
