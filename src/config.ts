971ec4eb97a2f5b616663a942239da7de85f5ad0
king-d-dev <davidoreal1@gmail.com>
2021-07-08 11:02:44 +0000

    dynamically setting socket url based on environment

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 const AUTH0_DOMAIN_NAME = 'dev-appoh.us.auth0.com';
 const AUTH0_CLIENT_ID = 'ABMIS1L5cahVgkVlnyHrQR6imMTdVj4J';
 const AUTH0_AUDIENCE = 'http://hola/api';
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = isDev ? 'http://localhost:5000' : 'https://hola-chat-server.herokuapp.com';
 
 export { AUTH0_DOMAIN_NAME, AUTH0_CLIENT_ID, SOCKET_URL, AUTH0_AUDIENCE };
